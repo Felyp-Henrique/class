20b9be984f32eab004794e1018522077217c50fa
Felyp-Henrique <felyphenrique2015@gmail.com>
2022-07-28 21:18:07 -0300

    code: making new class modifications

--- class.lua
+++ class.lua
@@ -1,178 +1,152 @@
-local Type
+local extends
 do
-    local _type = {}
-    _type.extend = function(self, class, ...)
-        for _, base in ipairs({...}) do
-            for static, default in pairs(base.__statics or {}) do
-                class.__statics = class.__statics or {}
-                class.__statics[static] = default
-            end
-            for field, default in pairs(base.__fields or {}) do
-                class.__fields = class.__fields or {}
-                class.__fields[field] = default
-            end
+  local _extends
+  _extends = function(target, src)
+    for field, value in pairs(src) do
+      if field == 'extends' then
+        for _, subclass in ipairs(value) do
+          _extends(target, subclass)
         end
+      elseif field == '__bases' then
+        for _, base in ipairs(value) do
+          table.insert(target.__bases, base)
+        end
+      elseif field ~= 'init' and field ~= '__index' then
+        target[field] = value
+      end
     end
-    _type.static = function(self, class, signature, value)
-        class.__statics = class.__statics or {}
-        class.__statics[signature] = value
+  end
+  extends = _extends
+end
+
+local object
+do
+  local _object
+  _object = {}
+  _object.__bases = {}
+  _object.to_string = function(self)
+    return tostring(self)
+  end
+  _object.clone = function(self)
+    local other = {}
+    other.__index = other
+    for field, value in pairs(self) do
+      if field ~= 'init' and field ~= '__index' then
+        other[field] = value
+      end
     end
-    _type.field = function(self, class, signature, default)
-        class.__fields = class.__fields or {};
-        class.__fields[signature] = default;
+    return setmetatable(other, other)
+  end
+  _object.equals = function(self, other)
+    local is_equals = true
+    -- first check if self is equals to other
+    for field, value in pairs(self) do
+      if type(value) ~= 'function' then
+        if other[field] ~= value then
+          is_equals = false
+        end
+      end
     end
-    _type.super = function(self, object)
-        return setmetatable({}, {
-            __index = function(_, index)
-                local method = nil
-                -- find constructors
-                if index == 'constructor' then
-                    local constructors = {}
-                    for _, base in ipairs(object.getBases()) do
-                        if base[index] ~= nil then
-                            if method == nil then
-                                method = base[index]
-                            end
-                            table.insert(constructors, base[index])
-                        end
-                    end
-                    if #constructors > 1 then
-                        error('Multiple inheritance conflict!')
-                        return nil
-                    end
-                    return function(...)
-                        return method(object, ...)
-                    end
-                end
-                -- find method in superclass list
-                for _, base in ipairs(object.getBases()) do
-                    method = base.__fields[index]
-                    if method and type(method) == 'function' then
-                        return function(...)
-                            return method(object, ...)
-                        end
-                    end
-                end
-                return nil
-            end
-        })
+    -- second check if other is equals to self
+    for field, value in pairs(other) do
+      if type(value) ~= 'function' then
+        if other[field] ~= value then
+          is_equals = false
+        end
+      end
     end
-    _type.toClass = function(self, class)
-        class.__index = function(table_, index)
-            local statics = rawget(table_, '__statics')
-            if statics and statics[index] then
-                return rawget(statics, index)
-            end
+    return is_equals
+  end
+  _object.__index = _object
+  object = setmetatable(_object, _object)
+end
+
+local class
+do
+  local _class
+  _class = function(definition)
+    local meta = definition or {}
+    local class = {}
+    class.__index = class
+    -- all class inherit from object
+    meta.extends = meta.extends or {}
+    table.insert(meta.extends, object)
+    class.__bases = meta.extends
+    -- it's the constructor
+    function class:__call(...)
+      local args = {...}
+      local object = setmetatable({}, self)
+      object.__index = object
+      for field, value in pairs(meta) do
+        if field == "init" then
+          value(object, table.unpack(args or {}))
+        else
+          object[field] = value
+        end
+      end
+      return object
+    end
+    -- do inheritance
+    for _, subclass in pairs(meta.extends or {}) do
+      extends(class, subclass)
+    end
+    -- it's the rest of class
+    for field, value in pairs(meta) do
+      -- it's the inheritance action
+      if field == 'extends' then
+        for subfield, subvalue in pairs(value) do
+          class[subfield] = subvalue
+        end
+      else
+        class[field] = value
+      end
+    end
+    return setmetatable(class, class)
+  end
+  class = _class
+end
+
+local super
+do
+  local _super
+  _super = function(object)
+    return setmetatable({}, {
+      __index = function(_, field)
+        for _, base in ipairs(object.__bases or {}) do
+          -- can't call constructors
+          if base[field] and field ~= 'init' then
+            error("Can't call constructor in super, use 'class.init(self, ...)' instead")
             return nil
-        end
-        class.new = function(self, ...)
-            local instance = {}
-            instance.getType = self.__statics.getType
-            instance.getBases = self.__statics.getBases
-            for field, default in pairs(self.__fields or {}) do
-                instance[field] = default
+          end
+          -- call method from base class
+          if base[field] and type(base[field]) == 'function' then
+            return function(...)
+              return base[field](object, ...)
             end
-            if class.constructor ~= nil then
-                class.constructor(instance, ...)
-            end
-            return instance
+          end
         end
-        class.__call = class.new
-        return setmetatable(class, class)
-    end
-    Type = _type
+        return nil
+      end,
+    })
+  end
+  super = _super
 end
 
-local Object
-do
-    local _object = {}
-    Type:static(_object, 'getType', function()
-        return "Object"
-    end)
-    Type:static(_object, 'getBases', function()
-        return {}
-    end)
-    Type:field(_object, 'clone', function(self)
-        local object = {}
-        for field, default in pairs(self) do
-            object[field] = default
-        end
-        return object
-    end)
-    Type:field(_object, 'equals', function(self, other)
-        if not other or not other.getType then
-            return false
-        end
-        if other:getType() ~= self:getType() or #other ~= #self then
-            return false
-        end
-        for field, self_value in pairs(self) do
-            if type(self_value) ~= 'function' and other[field] ~= self_value then
-                return false
-            end
-        end
-        return true
-    end)
-    Type:field(_object, 'toString', function(self)
-        return string.format("<%s>", self:getType())
-    end)
-    Object = Type:toClass(_object)
-end
-
-local ClassFactory
-do
-    local _factory = {}
-    _factory.simple = function(self, signature, definition)
-        definition = definition or {}
-        local class = {}
-        local bases = {Object, table.unpack(definition.extends or {})}
-        local statics = definition.statics or {}
-        Type:extend(class, table.unpack(bases))
-        for field, value in pairs(definition) do
-            if field == 'constructor' then
-                statics.constructor = value
-            elseif field ~= 'statics' then
-                Type:field(class, field, value)
-            end
-        end
-        statics.getType = function()
-            return signature
-        end
-        statics.getBases = function()
-            return bases
-        end
-        for static, value in pairs(statics) do
-            Type:static(class, static, value)
-        end
-        return Type:toClass(class)
-    end
-    _factory.global = function(self, signature, definition)
-        if _G[signature] ~= nil then
-            error('Class already defined!')
-        end
-        _G[signature] = self:simple(signature, definition)
-        return _G[signature]
-    end
-    ClassFactory = _factory
-end
-
-local function super(object)
-    return Type:super(object)
-end
-
-local function class(signature, definition)
-    return ClassFactory:simple(signature, definition)
-end
-
-local function gclass(signature, definition)
-    return ClassFactory:global(signature, definition)
-end
-
-return {
-    Type = Type,
-    Object = Object,
-    ClassFactory = ClassFactory,
-    class = class,
-    gclass = gclass,
-    super = super
+Pessoa = class {
+  init = function(self, nome, idade)
+    self.nome = nome or ""
+    self.idade = idade or 0
+  end
 }
+
+Funcionario = class {
+  extends = { Pessoa },
+  init = function(self, nome, idade, salary)
+    Pessoa.init(self, nome, idade)
+    self.salary = salary or 0
+  end
+}
+
+local funcionario = Funcionario("Joao", 30, 900.0)
+
+print(funcionario.nome, funcionario.idade, funcionario.salary)
