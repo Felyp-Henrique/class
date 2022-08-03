8eaa09d0f1d5f6b233e87c2b76c6f5162e809a66
Felyp-Henrique <felyphenrique2015@gmail.com>
2022-08-02 23:16:33 -0300

    test: wrotte unit test

--- test.lua
+++ test.lua
@@ -6,10 +6,12 @@ super = require('class').super
 
 function test.object_new()
   local obj = object:new()
+  assertNotNil(obj)
 end
 
 function test.object_to_string()
   local obj = object:new()
+  assertNotNil(obj:to_string())
 end
 
 function test.object_clone()
@@ -19,6 +21,7 @@ function test.object_clone()
   assertEqual(clone.y, 400)
   assertEqual(clone.x, obj.x)
   assertEqual(clone.y, obj.y)
+  assertNotEqual(clone, obj)
 end
 
 function test.object_equals()
@@ -30,6 +33,76 @@ function test.object_equals()
   assertEqual(obj2:equals(obj3), false)
   assertEqual(obj3:equals(obj2), false)
   assertEqual(obj1:equals(obj3), true)
+end
+
+function test.class()
+  local Point = class {
+    new = function (self, x, y)
+      self.x = x or 0
+      self.y = y or 0
+    end,
+    area = function (self)
+      return self.x * self.y
+    end
+  }
+
+  local point = Point:new(10, 20)
+
+  assertEqual(point.x, 10)
+  assertEqual(point.y, 20)
+  assertEqual(point:area(), 200)
+
+  local Zoom = class {
+    extends = { Point },
+    new = function (self, x, y, z)
+      self.x = x or 0
+      self.y = y or 0
+      self.z = z or 1
+    end,
+    zoom = function (self)
+      return self:area() * self.z
+    end,
+    to_string = function (self)
+      return tostring(self:zoom())
+    end
+  }
+
+  local zoom = Zoom:new(10, 20, 2)
+
+  assertEqual(zoom.x, 10)
+  assertEqual(zoom.y, 20)
+  assertEqual(zoom:area(), 200)
+  assertEqual(zoom:zoom(), 400)
+
+  assertNotEqual(zoom, point)
+  assertNotEqual(point, point:clone())
+  assertNotEqual(zoom, zoom:clone())
+
+  assertEqual(zoom:to_string(), '400')
+end
+
+function test.super()
+  local Point = class {
+    new = function (self, x, y)
+      self.x = x or 0
+      self.y = y or 0
+    end,
+    area = function (self)
+      return self.x * self.y
+    end
+  }
+  local Zoom = class {
+    extends = { Point },
+    new = function (self, x, y, z)
+      super(self, Point).new(x, y)
+      self.z = z or 1
+    end,
+    area = function (self)
+      return super(self):area() * self.z
+    end
+  }
+  local p = Zoom:new(10, 20, 2)
+  assertEqual(p:area(), 400)
 end
 
 test()
